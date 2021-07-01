907f3d6f94784090f2d515610da83d985f566370
Luca Panzavolta <lvolta@protonmail.com>
2021-07-01 11:31:21 +0200

    feat: Implemented logic to move the robot according to a set of instructions sent from Earth and get its new position.

---
+++ martianRobots.js
@@ -0,0 +1,134 @@
+
+/* 
+    The last position before a robot trips over the edge of the planet will be recorded in the "scents" object
+    in the following format: 
+
+    const scents = {
+        3: {
+            4: true
+        }
+    }
+
+    This way of storing data gives us a very convenient data structure to quickly access locations that left a scent.
+    In the example above "3" represents the x coordinate while "4" represents the y coordinate.
+*/
+
+
+function initializeScentDB() {
+  const scents = {} // initial state is an empty object
+  return scents;
+}
+
+function moveForward([x, y, orientation]) {
+  switch (orientation) {
+    case "N":
+      return [x, y + 1, orientation];
+    case "S":
+      return [x, y - 1, orientation];
+    case "E":
+      return [x + 1, y, orientation];
+    case "W":
+      return [x - 1, y, orientation];
+    default:
+      throw new Error("The orientation passed is not a valid value.");
+  }
+}
+
+function turnLeft([x, y, orientation]) {
+  switch (orientation) {
+    case "N":
+      return [x, y, "W"];
+    case "S":
+      return [x, y, "E"];
+    case "E":
+      return [x, y, "N"];
+    case "W":
+      return [x, y, "S"];
+    default:
+      throw new Error("The orientation passed is not a valid value.");
+  }
+}
+
+function turnRight([x, y, orientation]) {
+  switch (orientation) {
+    case "N":
+      return [x, y, "E"];
+    case "S":
+      return [x, y, "W"];
+    case "E":
+      return [x, y, "S"];
+    case "W":
+      return [x, y, "N"];
+    default:
+      throw new Error("The orientation passed is not a valid value.");
+  }
+}
+
+function isOutsideMatrix([x, y], [maxAllowedX, maxAllowedY]) {
+  if (x < 0 || y < 0) return true;
+  if (x > maxAllowedX || y > maxAllowedY) return true;
+  return false;
+}
+
+function mapInstructionToFunction(newInstruction) {
+  const map = {
+    "L": turnLeft,
+    "R": turnRight,
+    "F": moveForward
+  };
+
+  if (map[newInstruction]) return map[newInstruction];
+  throw new Error("Invalid Instruction.");
+}
+
+
+function processRobotInstructions([x, y, orientation], instructionString, matrixSize, scents) {
+  let isRobotLost = false;
+
+  const instructionsArray = instructionString.split("");
+
+  // Here we go through all the single instructions with a reduce algorithm
+  // the output of one operation constitutes the input for the following operation
+  // The initial state of the accumulator is set to [x, y, orientation] which represents the initial position of the robot before any move is made
+  // And then all instructions are executed in order and the final output the is the final position of the robot
+
+  const finalRobotPosition = instructionsArray.reduce((accumulator, instruction) => {
+    const moveRobot = mapInstructionToFunction(instruction);
+    const robotPositionAfterMove = moveRobot(accumulator);
+
+    if (!isRobotLost) {
+      const isRobotOutsideMatrix = isOutsideMatrix(robotPositionAfterMove, matrixSize);
+
+      if (isRobotOutsideMatrix) {
+        // let's check if we can ignore the current instruction because of a previous scent
+        const [currentX, currentY] = accumulator;
+        const doesCurrentPositionHaveScent = scents[currentX] ? scents[currentX][currentY] : undefined;
+
+        if (!doesCurrentPositionHaveScent) {
+          // we cannot ignore the current instruction, therefore the robot is from now on considered lost 
+          isRobotLost = true;
+          scents[currentX] = { [currentY]: true };
+        } else {
+          // there was a smell in the current position of the robot, so we can ignore the instruction and return current position
+          console.log(accumulator)
+          return accumulator
+        }
+      }
+    }
+
+    console.log(robotPositionAfterMove);
+    return robotPositionAfterMove;
+  }, [x, y, orientation]);
+
+  const result = isRobotLost ? finalRobotPosition.concat("LOST") : finalRobotPosition;
+  console.log({ result }, "\n\n\n");
+  return result;
+}
+
+module.exports = {
+  initializeScentDB,
+  moveForward,
+  turnLeft,
+  turnRight,
+  processRobotInstructions
+}
